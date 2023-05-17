ef55f2369fbd4e27f23363fa0a17d02a8276a0d7
Vinura de Silva <m.v.r.desilva@gmail.com>
2023-05-17 23:14:31 +0530

    Update [...nextauth].js

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['vinalmunasinghe@gmail.com'];
+const adminEmails = ['m.v.r.desilva@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
